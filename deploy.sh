c7ada76ed73a721c88878c8aa8e65dbf316d03d0
Richard StJohn <stjohn.rick@icloud.com>
2022-06-12 16:55:12 -0400

    temporarily add deploy script to feature branch

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp /Users/bigdata/repos/nifi-nar-bundles/nifi-asymmetrik-assembly/target/asymmetrik-nifi-nars-0.11.0-SNAPSHOT.tar.gz wolverine-nifi-1:
+scp /Users/bigdata/repos/nifi-nar-bundles/nifi-asymmetrik-assembly/target/asymmetrik-nifi-nars-0.11.0-SNAPSHOT.tar.gz wolverine-nifi-2:
+scp /Users/bigdata/repos/nifi-nar-bundles/nifi-asymmetrik-assembly/target/asymmetrik-nifi-nars-0.11.0-SNAPSHOT.tar.gz wolverine-nifi-3:
